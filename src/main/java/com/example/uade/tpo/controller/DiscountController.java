bb782badf12d4139de1ce063bb8e93bad7e37cf7
Olmos2121 <tomyolmos1@hotmail.com>
2024-06-03 04:34:53 -0300

    Cambio en DiscountController
    - Se agregaron comentarios en los metodos del DiscountController

--- src/main/java/com/example/uade/tpo/controller/DiscountController.java
+++ src/main/java/com/example/uade/tpo/controller/DiscountController.java
@@ -18,20 +18,20 @@ public class DiscountController {
     @Autowired
     private DiscountService discountService;
 
-    @GetMapping("/")
+    @GetMapping("/") //Get all discounts
     public ResponseEntity<List<DiscountResponseDto>> getDiscounts() {
         List<DiscountResponseDto> discounts = discountService.getDiscounts();
         return new ResponseEntity<>(discounts, HttpStatus.OK);
     }
 
-    @GetMapping("/{discountId}")
+    @GetMapping("/{discountId}") //Get discount by id
     public ResponseEntity<DiscountResponseDto> getDiscountById(@PathVariable Long discountId) {
         Optional<DiscountResponseDto> discount = discountService.getDiscount(discountId);
         return discount.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    @PostMapping
+    @PostMapping //Create discount
     public ResponseEntity<DiscountResponseDto> createDiscount(@RequestBody DiscountRequestDto discount) {
         DiscountResponseDto newDiscount = discountService.createDiscount(discount);
         if(newDiscount == null) {
@@ -40,7 +40,7 @@ public class DiscountController {
         return new ResponseEntity<>(newDiscount, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{discountId}")
+    @PutMapping("/{discountId}") //Update discount
     public ResponseEntity<DiscountResponseDto> updateDiscount(@PathVariable Long discountId, DiscountRequestDto discount) {
         DiscountResponseDto updatedDiscount = discountService.updateDiscount(discountId, discount);
         if(updatedDiscount == null) {
@@ -49,7 +49,7 @@ public class DiscountController {
         return new ResponseEntity<>(updatedDiscount, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{discountId}")
+    @DeleteMapping("/{discountId}") //Delete discount
     public ResponseEntity<Void> deleteDiscount(@PathVariable Long discountId) {
         Boolean deleted = discountService.deleteDiscount(discountId);
         if(!deleted) {
@@ -58,7 +58,7 @@ public class DiscountController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping("/{code}")
+    @DeleteMapping("/{code}") //Delete discount by code
     public ResponseEntity<Void> deleteDiscountByCode(@PathVariable String code) {
         Boolean deleted = discountService.deleteDiscountByCode(code);
         if(!deleted) {
